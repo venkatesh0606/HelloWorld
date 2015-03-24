c5bdcef208c6b3d57e18573d6b2be58584bca478
venkatesh0606 <venkatesh.devasani@gmail.com>
2015-03-24 10:44:18 +0530

    Project2

--- Project2/src/a/b/c/Project.java
+++ Project2/src/a/b/c/Project.java
@@ -6,7 +6,7 @@ public class Project {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("---2--2----Project1---to Projects-----");
+		System.out.println("---Project2-----");
 	}
 
 }
