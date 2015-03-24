20fb76086caa5d7967ca15b3bc33d53c5abd90db
venkatesh0606 <venkatesh.devasani@gmail.com>
2015-03-24 10:41:17 +0530

    2

---
+++ Project2/src/a/b/c/Project.java
@@ -0,0 +1,12 @@
+package a.b.c;
+
+public class Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("---2--2----Project1---to Projects-----");
+	}
+
+}
